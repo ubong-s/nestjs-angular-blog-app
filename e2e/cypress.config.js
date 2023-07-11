cc16ec660c0593bce66d7890386530591e4dd6f8
Ubong Sylvester <ubongsly@gmail.com>
2023-07-11 14:01:31 +0100

    cypress reports mochawesome

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      overwrite: false,
+      html: false,
+      json: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
